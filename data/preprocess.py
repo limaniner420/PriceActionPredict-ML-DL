7a70ae5f4bc83e2651d106a5b2e128f5ce36086e
Lima <56560333+limaniner420@users.noreply.github.com>
2022-10-29 15:29:07 -0700

    fixed const

--- data/preprocess.py
+++ data/preprocess.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
-SET_MIN = ["close" "symbol", "volume", "label", "change", "changePercent"]
-SET_ESS = ["close", "high", "low", "open", "symbol", "volume", "label", "change", "changePercent"]
+SET_MIN = ["date", "close" "symbol", "volume", "change", "changePercent"]
+SET_ESS = ["date", "close", "high", "low", "open", "symbol", "volume", "change", "changePercent"]
 
 def read_local(path: str, mode: str = "essential"):
     data = pd.read_json(path)
@@ -12,6 +12,3 @@ def read_local(path: str, mode: str = "essential"):
         data = data[SET_ESS]
 
     return data
-
-# def read_remote()
-# 'https://cloud.iexapis.com/stable/stock/SPY/chart/1y?token=pk_8ae6e7e04e0241348774a921c73e7629'
